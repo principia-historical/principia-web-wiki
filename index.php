b256300976b681a15d711d462cbb82e79e30b9c2
ROllerozxa <temporaryemail4meh+github@gmail.com>
2022-09-03 23:41:27 +0200

    Implement viewing older revisions

--- index.php
+++ index.php
@@ -2,12 +2,21 @@
 require('lib/common.php');
 
 $page = (isset($_GET['page']) ? str_replace('_', ' ', $_GET['page']) : 'Main Page');
+$revision = $_GET['rev'] ?? null;
 
-$pagedata = fetch("SELECT p.*, r.content FROM wikipages p JOIN wikirevisions r ON p.cur_revision = r.revision WHERE p.title = ? AND r.page = ?", [$page, $page]);
+if ($revision) {
+	$pagedata = fetch("SELECT p.*, r.content FROM wikipages p JOIN wikirevisions r ON ? = r.revision WHERE p.title = ? AND r.page = ?", [$revision, $page, $page]);
+
+	$revdata = fetch("SELECT $userfields r.* FROM wikirevisions r JOIN users u ON r.author = u.id WHERE r.page = ? AND r.revision = ?", [$page, $revision]);
+} else {
+	$pagedata = fetch("SELECT p.*, r.content FROM wikipages p JOIN wikirevisions r ON p.cur_revision = r.revision WHERE p.title = ? AND r.page = ?", [$page, $page]);
+}
 
 $twig = _twigloader();
 echo $twig->render('index.twig', [
 	'pagetitle' => $page,
 	'pagetitle_slugified' => str_replace(' ', '_', $page),
-	'page' => $pagedata
+	'page' => $pagedata,
+	'revision' => $revision,
+	'revdata' => $revdata ?? null
 ]);
