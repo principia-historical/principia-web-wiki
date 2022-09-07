d6b1a666032c2a305acfb8a146e5650dda56a1bf
ROllerozxa <temporaryemail4meh+github@gmail.com>
2022-09-07 22:28:30 +0200

    Remove commented out code

--- lib/parsing.php
+++ lib/parsing.php
@@ -6,20 +6,16 @@ function parsing($text) {
 
 	$text = $markdown->text($text);
 
-	//$text = preg_replace_callback('@{([\w\d\.]+) \"(.+)\"}@i', 'parseFunctions', $text);
 	$text = preg_replace_callback('@{{ ([\w\d\.]+)\((.+?)\) }}@si', 'parseFunctions', $text);
 
 	return $text;
 }
 
 function parseFunctions($match) {
-	//echo '<br><br><b>function debug:</b><br>';
-
 	$cmd = sprintf(
 		"luajit lib/lua/bootstrap.lua %s %s 2>&1",
 	escapeshellarg($match[1]), escapeshellarg($match[2]));
 
 	exec($cmd, $output);
-	//return '<hr>output:<br>'.$output.'<br><hr>error:<br>'.print_r($error, true).'<hr>';
 	return implode('', $output);
 }
