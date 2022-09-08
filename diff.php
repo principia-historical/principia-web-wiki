e1038de7677b01243659d16f725b0dd2a0f7a81f
ROllerozxa <temporaryemail4meh+github@gmail.com>
2022-09-08 22:16:15 +0200

    Page revision diff :D

---
+++ diff.php
@@ -0,0 +1,24 @@
+<?php
+require('lib/common.php');
+
+$page = (isset($_GET['page']) ? str_replace('_', ' ', $_GET['page']) : 'Main Page');
+$prev = $_GET['prev'] ?? null;
+$next = $_GET['next'] ?? null;
+
+$content['prev'] = fetch("SELECT content FROM wikirevisions WHERE page = ? AND revision = ?", [$page, $prev]);
+$content['next'] = fetch("SELECT content FROM wikirevisions WHERE page = ? AND revision = ?", [$page, $next]);
+
+$diff = new Diff(
+	explode("\n", $content['prev']['content']),
+	explode("\n", $content['next']['content']));
+$renderer = new Diff_Renderer_Html_Inline;
+$output = $diff->render($renderer);
+
+$twig = _twigloader();
+echo $twig->render('diff.twig', [
+	'pagetitle' => $page,
+	'pagetitle_slugified' => str_replace(' ', '_', $page),
+	'prev' => $prev,
+	'next' => $next,
+	'diff' => $output
+]);
