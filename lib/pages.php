69972238a7ad637127680c6eee8fdda8b7111aa2
ROllerozxa <temporaryemail4meh+github@gmail.com>
2022-09-24 17:11:15 +0200

    Magic trick to increase performance

--- lib/pages.php
+++ lib/pages.php
@@ -3,7 +3,7 @@
 function checkPageExistance($pagename) {
 	global $cache;
 
-	return $cache->hit('wpe_'.$pagename, function () use ($pagename) {
-		return result("SELECT COUNT(*) FROM wikipages WHERE BINARY title = ?", [$pagename]) == 1;
-	});
+	return $cache->hit('wpe_'.base64_encode($pagename), function () use ($pagename) {
+		return result("SELECT COUNT(*) FROM wikipages WHERE BINARY title = ?", [$pagename]);
+	}) == 1;
 }
