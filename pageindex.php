196cdcd4cb1e6850db63a55e4e5b54c32bc47f78
ROllerozxa <temporaryemail4meh+github@gmail.com>
2022-09-25 21:33:36 +0200

    Add page index with massive list of all pages

---
+++ pageindex.php
@@ -0,0 +1,9 @@
+<?php
+require('lib/common.php');
+
+$pages = query("SELECT title FROM wikipages ORDER BY title ASC");
+
+$twig = _twigloader();
+echo $twig->render('pageindex.twig', [
+	'pages' => $pages
+]);
