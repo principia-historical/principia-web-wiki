13d033a21e960f5a08c66e40af032a94b447579c
ROllerozxa <temporaryemail4meh+github@gmail.com>
2022-09-27 16:10:19 +0200

    Add Special:WantedPages

---
+++ special/wantedpages.php
@@ -0,0 +1,19 @@
+<?php
+$pagecontent = query("SELECT p.title, r.content FROM wikipages p JOIN wikirevisions r ON p.cur_revision = r.revision AND p.title = r.page ");
+
+$wantedpages = [];
+
+foreach ($pagecontent as $page) {
+	preg_match_all('/\[\[(.*?)\]\]/', $page['content'], $links);
+	foreach ($links[1] as $link){
+		if (!checkPageExistance($link)) {
+			if (!isset($wantedpages[$link])) $wantedpages[$link] = 0;
+			$wantedpages[$link]++;
+		}
+	}
+}
+
+$twig = _twigloader();
+echo $twig->render('wantedpages.twig', [
+	'wantedpages' => $wantedpages
+]);
