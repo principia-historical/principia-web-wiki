1309b91b42a5b407a52a3b0f13bb04342c6a6b33
ROllerozxa <rollerozxa@voxelmanip.se>
2022-10-12 21:08:41 +0200

    Add modification note to bottom, optimise index query

--- index.php
+++ index.php
@@ -22,11 +22,15 @@ if (str_starts_with($page, 'Special:')) {
 }
 
 if ($revision) {
-	$pagedata = fetch("SELECT p.*, r.content FROM wikipages p JOIN wikirevisions r ON ? = r.revision WHERE BINARY p.title = ? AND BINARY r.page = ?", [$revision, $page, $page]);
-
-	$revdata = fetch("SELECT $userfields r.* FROM wikirevisions r JOIN users u ON r.author = u.id WHERE BINARY r.page = ? AND r.revision = ?", [$page, $revision]);
+	$pagedata = fetch("SELECT p.*, $userfields r.time, r.content FROM wikipages p
+			JOIN wikirevisions r ON ? = r.revision
+			JOIN users u ON r.author = u.id
+			WHERE BINARY p.title = ? AND BINARY r.page = ? AND r.revision = ?", [$revision, $page, $page, $revision]);
 } else {
-	$pagedata = fetch("SELECT p.*, r.content FROM wikipages p JOIN wikirevisions r ON p.cur_revision = r.revision WHERE BINARY p.title = ? AND BINARY r.page = ?", [$page, $page]);
+	$pagedata = fetch("SELECT p.*, $userfields r.time, r.content FROM wikipages p
+			JOIN wikirevisions r ON p.cur_revision = r.revision
+			JOIN users u ON r.author = u.id
+			WHERE BINARY p.title = ? AND BINARY r.page = ?", [$page, $page]);
 }
 
 $twig = _twigloader();
@@ -34,6 +38,5 @@ echo $twig->render('index.twig', [
 	'pagetitle' => $page,
 	'pagetitle_slugified' => str_replace(' ', '_', $page),
 	'page' => $pagedata,
-	'revision' => $revision,
-	'revdata' => $revdata ?? null
+	'revision' => $revision
 ]);
