4e5f1c8bbc6e39278b42281bd3a9b46ad9d4aa82
ROllerozxa <rollerozxa@voxelmanip.se>
2022-10-12 21:33:35 +0200

    Improve wiki contributions page

--- special/contributions.php
+++ special/contributions.php
@@ -1,7 +1,7 @@
 <?php
 $id = $_GET['id'] ?? null;
 
-$user = fetch("SELECT * FROM users WHERE id = ?", [$_GET['id']]);
+$user = fetch("SELECT id, name, customcolor FROM users WHERE id = ?", [$_GET['id']]);
 
 if (!$user) error('404', 'Invalid user.');
 
@@ -9,9 +9,10 @@ $stats = [
 	'contributions' => 0,
 	'additions' => 0,
 	'removals' => 0,
+	'creations' => 0
 ];
 
-$revisions = query("SELECT id, size, sizediff FROM wikirevisions WHERE author = ?", [$id]);
+$revisions = query("SELECT id, revision, size, sizediff FROM wikirevisions WHERE author = ?", [$id]);
 
 while ($rev = $revisions->fetch()) {
 	$stats['contributions']++;
@@ -25,13 +26,17 @@ while ($rev = $revisions->fetch()) {
 		// 0 sizediff - page is new, add entire revision size to additions
 		$stats['additions'] += $rev['size'];
 	}
+
+	if ($rev['revision'] == 1)
+		$stats['creations']++;
 }
 
 $contrib_text = sprintf(
-	'%s has made %d contributions (<strong><span style="color:#00ff00">%s bytes added</span>, <span style="color:#ff0000">%s bytes removed</span></strong>)',
-userlink($user), $stats['contributions'], number_format($stats['additions'], 0, '', ' '), number_format($stats['removals'], 0, '', ' '));
+	'%s has made %d contributions and %d page creations
+		(<strong><span style="color:#00ff00">%s bytes added</span>, <span style="color:#ff0000">%s bytes removed</span></strong>)',
+userlink($user), $stats['contributions'], $stats['creations'], number_format($stats['additions'], 0, '', ' '), number_format($stats['removals'], 0, '', ' '));
 
-$contributions = query("SELECT page, revision, size, sizediff, time, description FROM wikirevisions WHERE author = ? ORDER BY time DESC, id DESC LIMIT 50", [$id]);
+$contributions = query("SELECT page, revision, size, sizediff, time, description FROM wikirevisions WHERE BINARY author = ? ORDER BY time DESC, id DESC LIMIT 50", [$id]);
 
 $twig = _twigloader();
 echo $twig->render('contributions.twig', [
